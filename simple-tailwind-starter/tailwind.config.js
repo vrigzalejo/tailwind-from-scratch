a37d19cd357a3ec18c47df04c0b16fa6ac76075e
Vrigz Alejo <brigsalejo@gmail.com>
2025-04-11 00:36:21 -0700

    [Sec 4] Downgraded tailwind to v3 and refactored

--- simple-tailwind-starter/tailwind.config.js
+++ simple-tailwind-starter/tailwind.config.js
@@ -1,11 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./*.html'],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
 }
+
