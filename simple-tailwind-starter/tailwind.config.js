060d78c11161965d92c2cfa025c20947112ed572
Vrigz Alejo <brigsalejo@gmail.com>
2025-04-11 01:30:07 -0700

    [Sec 4] 26

--- simple-tailwind-starter/tailwind.config.js
+++ simple-tailwind-starter/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        128: '32rem',
+        144: '36rem',
+      }
+    },
   },
   plugins: [],
 }
