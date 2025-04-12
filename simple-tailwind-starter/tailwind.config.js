3532ad0e33fa93b2da61bc376b588880aa9b9294
Brigs Alejo <brigsalejo@gmail.com>
2025-04-12 04:09:51 -0700

    [Sec 4] 25-27 (#1)
    * [Sec 4] 25
    
    * [Sec 4] Downgraded tailwind to v3 and refactored
    
    * [Sec 4] 26
    
    * [Sec 4] 27

---
+++ simple-tailwind-starter/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      spacing: {
+        128: '32rem',
+        144: '36rem',
+      }
+    },
+  },
+  plugins: [],
+}
+
